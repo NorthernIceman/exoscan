2c50b0e2f88ae0921a7fae8d02830bc8b115d680
is231516 <is231516@fhstp.ac.at>
2025-03-17 00:06:03 +0100

    Made a test to interact with Exoscale API and implemented provider which returns auth-header for requests.

--- exoscan/__main__.py
+++ exoscan/__main__.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from provider.exoscale_provider import authenticate
 from log_conf.logger import logger, set_logging_config
+import requests
 
 def exoscan():
     set_logging_config()
     logger.info("Trying to log into exoscale with provided credentials...")
-    authenticate()
-        
+    auth = authenticate()
+    response = requests.get("https://api-ch-gva-2.exoscale.com/v2/ssh-key", auth=auth)
+    print(response.json())
+      
     #ToDo: 
     # Take creds, 
     # Use api: https://github.com/exoscale/python-exoscale/actions
--- provider/exoscale_provider.py
+++ provider/exoscale_provider.py
@@ -1,18 +1,26 @@
+import sys
 from log_conf.logger import logger 
+from exoscale_auth import ExoscaleV2Auth
 
-def check_file_for_string(file_path, target_string="XXXXXX"):
-    with open(file_path, "r", encoding="utf-8") as file:
-        return target_string in file.read()
-
-def authenticate():
-    print("Hello from exoscale_provider.authenticate")
-
+def authenticate(): #returns the authentication header after parsing the file content
     file_path = "provider/credentials.txt" 
-    if check_file_for_string(file_path):
-        logger.info("No credentials supplied! Please submit them in provider/credentials.txt")
-        return 
-    else:
-        print("ToDo")
-        return 0
+    try:
+        file = open(file_path, "r")
+        api_key_line = file.readline()
+        api_secret_line = file.readline()
+        if ("XXXXXX" in (api_key_line or api_secret_line)):
+            logger.info("No credentials supplied! Please submit them in provider/credentials.txt")
+            return
+        else:
+            API_KEY = api_key_line.split("=")[1].strip().strip('"').strip("'")
+            API_SECRET = api_secret_line.split("=")[1].strip().strip('"').strip("'")
+            auth = ExoscaleV2Auth(API_KEY, API_SECRET)
+            return auth
+    except (FileExistsError, FileNotFoundError):
+        logger.error("File could not be opened or does not exist.")
+        sys.exit("Authentication file not found. Abort...")
+
+
+    
 
 
